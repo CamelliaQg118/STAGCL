655dc5f678818bb3ae58d67f2fae69e9d70af1b4
QiGao <147978754+CamelliaQg118@users.noreply.github.com>
2026-03-10 21:32:32 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 from .utils import fix_seed
 from .STAGCL_model import stagcl
-from .STAGCL_model_degs import stagcl_degs
 from .clustering import mclust_R, leiden, louvain
 
 # from module import *
 __all__ = [
     "fix_seed",
     "stagcl",
-    "stagcl_degs",
+    "stagcl",
     "mclust_R"
 ]
+
 
